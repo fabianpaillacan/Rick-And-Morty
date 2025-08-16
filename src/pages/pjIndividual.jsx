8c2a4e03e0c8a1567acd723c0fbf741c8f9397ec
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-15 23:19:59 -0400

    Se creó el componente Personaje que incluye un botón para regresar a la lista de personajes y una estructura básica para su visualización.

---
+++ src/pages/pjIndividual.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css'
+
+function Personaje() {
+
+  return (
+    <div>
+        <div
+            className="bg-white text-purple-600 px-8 py-4 rounded-lg font-bold hover:bg-blue-900  hover:text-white transition-colors absolute z-10"        >
+            <Link
+            to="/personajes"
+            >
+             Volver
+            </Link>
+        </div>
+        <div className='min-h-screen flex items-center justify-center'>
+          
+        </div>
+    </div>
+  );
+}
+
+export default Personaje;
