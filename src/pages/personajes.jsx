a0b6a4033cc22cdd0b0e2e9ca91c92849bd55c24
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-15 23:19:49 -0400

    Se implementó la carga de personajes desde la API en el componente Personajes y se mejoró la estructura del renderizado para mostrar los nombres de los personajes en una cuadrícula.

--- src/pages/personajes.jsx
+++ src/pages/personajes.jsx
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css'
+import { getDatos } from '../services/api';
 
 function Personajes() {
+    const [personajes, setPersonajes] = useState([]);
 
+    useEffect(() => {
+        cargarPersonajes();
+      }, []);
+
+    const cargarPersonajes= async () => {
+        try {
+          const data = await getDatos();
+ 
+          setPersonajes(data.results);
+        } catch (error) {
+          console.error('Error loading pokemons:', error);
+        } 
+      };
   return (
     <div>
         <div
-            className="bg-white text-purple-600 px-8 py-4 rounded-lg font-bold hover:bg-blue-900  hover:text-white transition-colors absolute z-10"        >
+            className="bg-white text-purple-600 px-8 py-4 rounded-lg font-bold hover:bg-blue-900  hover:text-white transition-colors"        >
             <Link
             to="/"
             >
              Volver
             </Link>
         </div>
-        <div className='min-h-screen flex items-center justify-center'>
-          a
+        <div className='grid grid-cols-2 sm:grid-cols-3 gap-5 mt-6'>
+            {personajes.map((pj) => (
+            <div key={pj.id} className="">
+                {pj.name}
+            </div>
+            ))}
         </div>
     </div>
   );
