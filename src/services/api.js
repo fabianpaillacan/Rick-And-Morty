75c83c811db6e49f129f0099e6eed096dc594fee
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-16 21:16:56 -0400

    Se añadió la función getLocations para obtener datos de ubicaciones desde la API de Rick y Morty, mejorando la funcionalidad del servicio. También se corrigió el formato de la constante URL y se añadió un salto de línea al final del archivo.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
-const URL = "https://rickandmortyapi.com/api/character/?page=1" //puede ser page=1, 2,3,4,5,6 etc
+const URL = "https://rickandmortyapi.com/api/character/?page=1"; //puede ser page=1, 2,3,4,5,6 etc
+const LOCATION = "https://rickandmortyapi.com/api/location?page=1";
+
 
 export async function getDatos() {
     const respuesta = await fetch(URL);
     if (!respuesta.ok) throw new Error('Error al obtener datos');
     return respuesta.json();
   }
+
+export async function getLocations() {
+  const ubicaciones = await fetch(LOCATION);
+  if (!ubicaciones.ok) throw new Error('Error al obtener datos');
+  return ubicaciones.json();
+}
