a8dbab0d47da2c356fea897a512b888e0374a968
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-16 21:16:40 -0400

    Se mejoró el componente Personajes añadiendo navegación a los detalles de cada personaje, actualizando estilos y mostrando la especie de cada personaje. Se implementó un diseño más responsivo y atractivo.

--- src/pages/personajes.jsx
+++ src/pages/personajes.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../index.css'
 import { getDatos } from '../services/api';
 
 function Personajes() {
+    const navigate = useNavigate();
     const [personajes, setPersonajes] = useState([]);
 
     useEffect(() => {
@@ -20,27 +21,34 @@ function Personajes() {
         } 
       };
   return (
-    <div>
+    <div className='bg-purple-100'>
         <div
-            className="bg-white text-purple-600 px-8 py-4 rounded-lg font-bold hover:bg-blue-900  hover:text-white transition-colors"        >
+            className="bg-teal-400 text-black px-6 py-8 ml-3 rounded-lg font-bold hover:bg-blue-900 hover:text-white transition-colors w-fit">
             <Link
             to="/"
+            className="block text-center"
             >
              Volver
             </Link>
         </div>
-        <div className='grid grid-cols-2 sm:grid-cols-3 gap-5 mt-6'>
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-5 mt-6'>
             {personajes.map((pj, index) => (
-            <button key={pj.id} className="border-t-indigo-950 bg-gray-600 rounded-bl-3xl rounded-tr-3xl">
+                         <button key={pj.id} 
+                     className="border-2 border-orange-950  rounded-bl-3xl rounded-tr-3xl hover:bg-teal-600 transition-all duration-200 hover:scale-90"
+                     onClick={() => navigate(`/personajes/${pj.id}`)}
+                     >
                   <div>
                     <img 
                             src={`https://rickandmortyapi.com/api/character/avatar/${index+1}.jpeg`}
                             alt={pj.name}
-                            className="w-40 h-40 mt-3 mx-auto relative z-10 filter contrast-125 brightness-110 drop-shadow-lg group-hover:drop-shadow-2xl transition-all duration-300"
+                            className="w-40 h-40 mt-3 mx-auto rounded-xl relative z-10 shadow-xl/30 shadow-black transition-all duration-300"
                           />
                   </div>
                   <div>
-                    <h2 className="text-2xl font-extralight text-white">{pj.name}</h2>
+                    <h2 className="text-2xl font-bold font-mono text-black">{pj.name}</h2>
+                  </div>
+                  <div>
+                    <h4>{pj.species}</h4>
                   </div>
             </button>
             ))}
